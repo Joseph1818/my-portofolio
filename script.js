b3e9eaf9b34867c9c750afd3f959a4d03e27a5c1
Umba joseph <Josephdjamba04@gmail.com>
2024-05-03 20:06:35 +0200

    About section: Picture section transition

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".about-pic");
+  for (let i = 1; i < slides.length; i++) {
+    slides[i].style.display = "none"; // Hide all images except the first one
+  }
+
+  let currentSlide = 0;
+
+  function showSlide(n) {
+    slides[currentSlide].style.display = "none";
+    currentSlide = (currentSlide + n + slides.length) % slides.length;
+    slides[currentSlide].style.display = "block";
+  }
+
+  function nextSlide() {
+    showSlide(1);
+  }
+
+  setInterval(nextSlide, 2000);
+});
